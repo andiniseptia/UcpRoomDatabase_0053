ee5c1e61beac5f48c34d5a7b224fed6708c9ae29
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 00:02:32 +0700

    Menambahkan inisialisasi uiState di HomeTokoViewModel

--- app/src/main/java/com/example/pam_ucp2_andini_053/ui/viewmodel/HomeTokoViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/viewmodel/HomeTokoViewModel.kt
@@ -6,6 +6,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 
+class HomeTokoViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(HomeTokoUiState())
+    val uiState: StateFlow<HomeTokoUiState> = _uiState
+
+
+}
 
 data class HomeTokoUiState(
     val navigateToBarang: Boolean = false,
