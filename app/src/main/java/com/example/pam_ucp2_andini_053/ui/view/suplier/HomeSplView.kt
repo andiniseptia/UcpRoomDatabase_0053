14146051697bd27fec6f2a6a4290d76a5b398170
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 02:35:08 +0700

    Menambahkan card suplier

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/view/suplier/HomeSplView.kt
@@ -0,0 +1,115 @@
+package com.example.pam_ucp2_andini_053.ui.view.suplier
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2_andini_053.R
+import com.example.pam_ucp2_andini_053.data.entity.Suplier
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.HomeSplViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.HomeUiStateSpl
+import kotlinx.coroutines.launch
+import com.example.pam_ucp2_andini_053.ui.customwidget.TopAppBar
+import com.example.pam_ucp2_andini_053.ui.viewmodel.PenyediaViewModel
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardSpl(
+    spl: Suplier,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    val cardColor = Color.Gray
+    Card(
+        onClick = onClick,
+        colors = CardDefaults.cardColors(containerColor = cardColor),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp),
+        ) {
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.namaSuplier,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Call, contentDescription = "", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.kontak,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.alamat,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
+}
