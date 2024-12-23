2822f7fbb982276c300ce8335b020a96b8679645
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 01:00:11 +0700

    menambahkan fungsi TokoApp untuk mengakses dari CreationExtras

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pam_ucp2_andini_053.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pam_ucp2_andini_053.TokoApp
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.BarangViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.DetailBrgViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.HomeBrgViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.UpdateBrgViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.HomeSplViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.SuplierViewModel
+
+
+fun CreationExtras.tokoApp(): TokoApp = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TokoApp)
