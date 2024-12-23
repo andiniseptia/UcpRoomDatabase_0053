b9454af71fc88923d9bf9f21ae360e251fc62289
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 02:43:09 +0700

    Mengubah greeting menjadi pengelola halaman

--- app/src/main/java/com/example/pam_ucp2_andini_053/MainActivity.kt
+++ app/src/main/java/com/example/pam_ucp2_andini_053/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam_ucp2_andini_053.ui.navigation.PengelolaHalaman
 import com.example.pam_ucp2_andini_053.ui.theme.PAMUCP2Andini053Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PAMUCP2Andini053Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
