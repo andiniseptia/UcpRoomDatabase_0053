8bf16908a66686b1cd1c8dd9fc7fa33417501dcb
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 02:37:22 +0700

    Menambahkan object destinasi insert suplier

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/view/suplier/InsertSplView.kt
@@ -0,0 +1,41 @@
+package com.example.pam_ucp2_andini_053.ui.view.suplier
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2_andini_053.R
+import com.example.pam_ucp2_andini_053.ui.navigation.AlamatNavigasi
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.FormErrorStateSpl
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.HomeSplViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.SplUIState
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.SuplierEvent
+import com.example.pam_ucp2_andini_053.ui.viewmodel.suplier.SuplierViewModel
+import kotlinx.coroutines.launch
+import com.example.pam_ucp2_andini_053.ui.customwidget.TopAppBar
+import com.example.pam_ucp2_andini_053.ui.viewmodel.PenyediaViewModel
+
+object DestinasiInsertSpl : AlamatNavigasi {
+    override val route: String = "insert_spl"
+}
+
