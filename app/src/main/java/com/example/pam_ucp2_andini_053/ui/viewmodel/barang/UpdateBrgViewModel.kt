f71deb4d80219449f8b0b4b32befb17e13a49716
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 00:29:38 +0700

    Menambahkan konversi Barang ke BrgUIState

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/viewmodel/barang/UpdateBrgViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.pam_ucp2_andini_053.ui.viewmodel.barang
+
+import android.util.Log
+import android.view.View
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_ucp2_andini_053.data.NamaSuplier
+import com.example.pam_ucp2_andini_053.data.entity.Barang
+import com.example.pam_ucp2_andini_053.repository.RepositoryBrg
+import com.example.pam_ucp2_andini_053.ui.navigation.DestinasiUpdateBarang
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+
+fun Barang.toUIStateBrg(): BrgUIState = BrgUIState(
+    barangEvent = this.toDetailUiEvent(),
+)
