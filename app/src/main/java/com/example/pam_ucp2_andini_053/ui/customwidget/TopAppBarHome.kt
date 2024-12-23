5b44d6723b17c7342fddeae6cfdb3a1e1ac6b5cd
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 02:16:39 +0700

    Menambahkan fungsi Top App Bar Home

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/customwidget/TopAppBarHome.kt
@@ -0,0 +1,45 @@
+package com.example.pam_ucp2_andini_053.ui.customwidget
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pam_ucp2_andini_053.R
+
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarHome(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 40.dp)
+
+            .background(color = colorResource(id = R.color.primary), shape = RoundedCornerShape(bottomEnd = 60.dp)
+            )
+    )
+    {
+
+    }
+}
