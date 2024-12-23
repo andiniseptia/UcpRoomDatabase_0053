43468bdb21c379439aaaf963ecebb8a1c478270c
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 00:14:36 +0700

    Menambahkan data class DetailUiState

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.pam_ucp2_andini_053.ui.viewmodel.barang
+
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_ucp2_andini_053.data.entity.Barang
+import com.example.pam_ucp2_andini_053.repository.RepositoryBrg
+import com.example.pam_ucp2_andini_053.ui.navigation.DestinasiDetailBarang
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+
+data class DetailUiState(
+    val detailUiEvent: BarangEvent = BarangEvent(),
+    val isLoading: Boolean = false,
+    val isError:Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == BarangEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != BarangEvent()
+}
+
+fun Barang.toDetailUiEvent(): BarangEvent {
+    return BarangEvent(
+        id = id,
+        nama = nama,
+        deskripsi = deskripsi,
+        harga = harga,
+        stok = stok,
+        namaSuplier = namaSuplier
+    )
+}
