84494d7518675ac7f43e288b4e1ddf0509928436
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 01:05:22 +0700

    Menambahkan object destinasi navigasi home

--- app/src/main/java/com/example/pam_ucp2_andini_053/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,8 @@ package com.example.pam_ucp2_andini_053.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
