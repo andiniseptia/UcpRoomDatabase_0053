1b9c044ed01a61b57e88bac2731ddd2fa41b2ce4
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 02:22:38 +0700

    membuat object destinasi insert barang

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/view/barang/InsertBrgView.kt
@@ -0,0 +1,56 @@
+package com.example.pam_ucp2_andini_053.ui.view.barang
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2_andini_053.R
+import com.example.pam_ucp2_andini_053.data.NamaSuplier
+import com.example.pam_ucp2_andini_053.repository.RepositoryBrg
+import com.example.pam_ucp2_andini_053.ui.navigation.AlamatNavigasi
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.BarangEvent
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.BarangViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.BrgUIState
+import kotlinx.coroutines.launch
+import java.text.Normalizer.Form
+import com.example.pam_ucp2_andini_053.ui.customwidget.TopAppBar
+import com.example.pam_ucp2_andini_053.ui.viewmodel.PenyediaViewModel
+import com.example.pam_ucp2_andini_053.ui.viewmodel.barang.FormErrorStateBrg
+
+object DestinasiInsert : AlamatNavigasi {
+    override val route: String = "insert_brg"
+}
+
