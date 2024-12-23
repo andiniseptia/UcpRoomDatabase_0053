a0884b1b358c636494913afd6ec6462a53490f4d
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-23 23:49:30 +0700

    Implementasi fungsi repository suplier

--- app/src/main/java/com/example/pam_ucp2_andini_053/repository/LocalRepositorySpl.kt
+++ app/src/main/java/com/example/pam_ucp2_andini_053/repository/LocalRepositorySpl.kt
@@ -8,5 +8,16 @@ import kotlinx.coroutines.flow.Flow
 class LocalRepositorySpl (
     private val suplierDao: SuplierDao
 ) : RepositorySpl {
+    override suspend fun insertSuplier(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+
+    override fun getAllSuplier(): Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
+    }
+
+    override fun getSuplier(idSuplier: Int): Flow<Suplier> {
+        return suplierDao.getSuplier(idSuplier)
+    }
 
 }
