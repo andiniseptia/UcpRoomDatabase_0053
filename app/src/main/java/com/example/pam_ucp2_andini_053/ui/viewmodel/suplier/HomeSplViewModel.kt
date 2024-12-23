80dd519835102c9fb61c72188fc0c5526793d1b2
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-24 00:42:17 +0700

    Menambahkan data class HomeUiStateSpl

---
+++ app/src/main/java/com/example/pam_ucp2_andini_053/ui/viewmodel/suplier/HomeSplViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.pam_ucp2_andini_053.ui.viewmodel.suplier
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_ucp2_andini_053.data.entity.Suplier
+import com.example.pam_ucp2_andini_053.repository.RepositorySpl
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+
+
+data class HomeUiStateSpl (
+    val listSpl: List<Suplier> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
